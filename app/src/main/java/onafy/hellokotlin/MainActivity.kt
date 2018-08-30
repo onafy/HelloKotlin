c2889c385d277774fa232f849bf1764ed888b30e
azizahnovitami <ccghy@gmail.com>
2018-08-30 21:05:46 +0800

    tambah adapter

---
+++ app/src/main/java/onafy/hellokotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package onafy.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textHello = findViewById<TextView>(R.id.text_hello)
+        textHello.text = "Hello Kotlin!"
+    }
+}
---
+++ app/src/main/java/onafy/hellokotlin/model/Item.kt
@@ -0,0 +1,3 @@
+package onafy.hellokotlin.model
+
+data class Item (val name: String?, val image: Int?)
